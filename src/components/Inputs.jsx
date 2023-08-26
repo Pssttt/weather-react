dfeaf0dcc596f37655530d9f7cde3e4008ff44dc
Pssttt <pssthant@gmail.com>
2023-08-26 23:20:35 +0630

    remove toast from inputs

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
-import { toast } from "react-toastify";
 
 function Inputs({ setQuery, units, setUnits }) {
   const [city, setCity] = useState("");
